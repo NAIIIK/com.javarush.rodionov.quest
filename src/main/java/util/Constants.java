cd46ca1c63520f23b859c6ea10fd5f40e50d470b
RodionovNikita <radzivonaumikita@gmail.com>
2026-03-29 18:47:10 +0200

    added constants with full paths to jsp's

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -5,4 +5,7 @@ public final class Constants {
     private Constants() {}
 
     public static final String QUESTIONS_FILE_NAME = "castleGame.json";
+    public static final String NICKNAME_ATTRIBUTE = "nickname";
+    public static final String GAME_VIEW = "/WEB-INF/views/game.jsp";
+    public static final String START_VIEW = "/WEB-INF/views/start.jsp";
 }
